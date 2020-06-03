be444684404c32a9f5aa8be3fd7895da8008552e
thiago Siqueira <thiagofelipesiqueira@gmail.com>
2020-06-03 14:52:28 -0300

    Implementation of error handling

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,22 +5,18 @@ import CreateSessionsUserService from '../services/CreateSessionsUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const autenticateUser = new CreateSessionsUserService();
+  const autenticateUser = new CreateSessionsUserService();
 
-    const { user, token } = await autenticateUser.execute({
-      email,
-      password,
-    });
+  const { user, token } = await autenticateUser.execute({
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json({ user, token });
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json({ user, token });
 });
 
 export default sessionsRouter;
